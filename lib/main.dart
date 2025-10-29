a0cb2c55cce33ee342cbd0377ffdb5df03aca5ab
TEJAS MS <tejastejasms71@gmail.com>
2025-10-29 20:50:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:protiencaluclator/fooddetails_page.dart';
+
 import 'package:protiencaluclator/home_page.dart';
-import 'package:protiencaluclator/global_varialbles.dart';
-import 'package:protiencaluclator/fooddetails_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +18,7 @@ class MyApp extends StatelessWidget {
           primary: Color.fromARGB(255, 105, 122, 179),
         ),
       ),
-      home: HomePage(),
+      home: HomePage(product: []),
     );
   }
 }
