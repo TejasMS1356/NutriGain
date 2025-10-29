3bb5e6edc839888a77d1d0836c383a2d10e4c012
TEJAS MS <tejastejasms71@gmail.com>
2025-10-29 20:49:11 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:protiencaluclator/food_card.dart';
 import 'package:protiencaluclator/global_varialbles.dart';
 import 'package:protiencaluclator/fooddetails_page.dart';
+import 'package:protiencaluclator/protien_count.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  final List<Map<String, dynamic>> product;
+  const HomePage({super.key, required this.product});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController sc = TextEditingController();
+  List<Map<String, dynamic>> fps = [];
+  List<bool> checkedItems = [];
   final List<String> categories = [
     'All',
     'Vegetables',
@@ -19,7 +24,26 @@ class _HomePageState extends State<HomePage> {
     'non veg',
     'Dairy',
   ];
+  @override
+  void initState() {
+    super.initState();
+    fps = products;
+    checkedItems = List<bool>.filled(fps.length, false);
+  }
+
+  int currentpage = 1;
   late String selected_category = categories[0];
+  void filterProducts(String query) {
+    setState(() {
+      fps =
+          products.where((product) {
+            final name = product['name'].toString().toLowerCase();
+            return name.contains(query.toLowerCase());
+          }).toList();
+      checkedItems = List<bool>.filled(fps.length, false);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +63,20 @@ class _HomePageState extends State<HomePage> {
           Icons.fitness_center,
           color: Color.fromARGB(255, 208, 206, 221),
         ),
+        // In home_page.dart, inside AppBar
+        actions: [
+          IconButton(
+            icon: Icon(Icons.today),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FooddetailsPage(product: products[0]),
+                ),
+              );
+            },
+          ),
+        ],
       ),
       body: SafeArea(
         child: Column(
@@ -48,6 +86,9 @@ class _HomePageState extends State<HomePage> {
                 Padding(padding: EdgeInsets.symmetric(vertical: 45)),
                 Expanded(
                   child: TextField(
+                    controller: sc,
+                    onChanged: filterProducts,
+
                     decoration: InputDecoration(
                       hintText: "search",
 
@@ -55,6 +96,7 @@ class _HomePageState extends State<HomePage> {
                         fontWeight: FontWeight.bold,
                         color: Color.fromRGBO(54, 47, 47, 1),
                       ),
+
                       prefixIcon: Icon(Icons.search),
                       prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
                       border: OutlineInputBorder(
@@ -110,15 +152,18 @@ class _HomePageState extends State<HomePage> {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: products.length,
+                itemCount: fps.length,
                 itemBuilder: (context, index) {
-                  final product = products[index];
+                  final product = fps[index];
+
                   return GestureDetector(
                     onTap: () {
                       Navigator.of(context).push(
                         MaterialPageRoute(
                           builder: (context) {
-                            return FooddetailsPage(product: product);
+                            return FooddetailsPage(
+                              product: product.cast<String, Object>(),
+                            );
                           },
                         ),
                       );
@@ -126,6 +171,8 @@ class _HomePageState extends State<HomePage> {
                     child: FoodCard(
                       name: product['name'] as String,
                       image: product['imageUrl'] as String,
+                      protein: (product['protein'] as int).toDouble(),
+                      calories: (product['calories'] as int).toDouble(),
                       backgroundcolor:
                           index.isEven
                               ? Color.fromRGBO(72, 129, 160, 1)
@@ -138,6 +185,27 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // Always show Home as selec   ted
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ProtienCount(product: products),
+              ),
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: "Protein count",
+          ),
+        ],
+      ),
     );
   }
 }
+
